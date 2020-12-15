531401d2acda3bc46c9205edb6da12ec741351db
serkankisacik <serkankisacik01@gmail.com>
2020-12-15 23:05:36 +0300

    Theme Folder Created

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>TechNews - HTML and CSS Template</title>
+    <title>Ulag News - Yurttan Dünyadan Haberler</title>
 
     <!-- favicon -->
     <link href="{{asset('assets')}}/img/favicon.png" rel=icon>
