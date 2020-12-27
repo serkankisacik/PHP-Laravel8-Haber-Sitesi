f8c288060dc23ed2ca1ebc3b496b86cf18a15143
serkankisacik <serkankisacik01@gmail.com>
2020-12-27 11:52:01 +0300

    Admin Login Operations

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -11,5 +12,37 @@ class HomeController extends Controller
     public function index()
     {
         return view('admin.index');
+    }
+
+    public function login()
+    {
+        return view('admin.login');
+    }
+
+    public function logincheck(Request $request)
+    {
+        if ($request->isMethod('post')){
+            $credentials = $request->only('email','password');
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('admin');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+        }
+        else{
+            return view('admin.login');
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,10 @@ Route::get('/', function () {
 
 Route::get('/home', [HomeController::class, 'index']);
 
-Route::get('/admin', [\App\Http\Controllers\Admin\HomeController::class, 'index']);
+Route::get('/admin', [\App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin_home')->middleware('auth');
+Route::get('/admin/login', [\App\Http\Controllers\Admin\HomeController::class, 'login'])->name('admin_login');
+Route::post('/admin/logincheck', [\App\Http\Controllers\Admin\HomeController::class, 'logincheck'])->name('admin_logincheck');
+Route::get('/admin/logout', [\App\Http\Controllers\Admin\HomeController::class, 'logout'])->name('admin_logout');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
