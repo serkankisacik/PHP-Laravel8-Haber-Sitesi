e931d5172e249a17789168f7f3dacad0422ac0fe
serkankisacik <serkankisacik01@gmail.com>
2021-01-10 09:50:52 +0300

    try vol II

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -8,7 +8,6 @@ use App\Models\News;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 
 class NewsController extends Controller
 {
